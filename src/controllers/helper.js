39e721f92a2ad203e21f65d1846d7a61f6af54f7
Yash Gangwar <gangwaryash06@gmail.com>
2024-06-25 02:15:21 +0530

    fixed prompt

--- src/controllers/helper.js
+++ src/controllers/helper.js
@@ -194,7 +194,7 @@ const extractTextFromPdf = async (buffer) => {
 }
 
 exports.modelprompt = `Extract Invoice number/Reference Number/REF# as invoiceNumber, Vendor Name the company that produced invoice as vendor, Invoice/Billing Date as invoiceDate, Total of items without any taxes or additional charges as total, Total payable amount after taxes as totalPayable and list of items named ingredients as array of objects with fields in which take description/item as name, Qty/Quantity as quantity, Unit Price per quantity as unitPrice, total Amount of particular item as total. Also return just JSON format in pretty format with no additional texts.
-                       Imp: dont return any numerical value with commas in it like 3,240 should be 3240
+                       Imp: dont return any numerical value with commas or brackets or dollar signs in it. Eg. $3,240.97 should be just 3240.97
                         {
                             invoiceNumber: ''
                             vendor: '',
@@ -208,7 +208,7 @@ exports.modelprompt = `Extract Invoice number/Reference Number/REF# as invoiceNu
                                 },
                             ]
                             total: 'total before any additional charges/discounts'
-                            totalPayable: 'payable amt after all taxes/discounts, or after adding previous balance or deducting already paid amount'  '
+                            totalPayable: 'should not be empty, it should be payable amt after all taxes/discounts, or after adding previous balance or deducting already paid amount or deducting the amount already paid as retainer'
                         }
                     `
 
